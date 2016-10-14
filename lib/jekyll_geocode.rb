c7d9d15cda919ac3e20ee7b862daa644fe29135a
bk <bertrand.keller@gmail.com>
2016-10-14 11:34:47 +0200

    add code for removing duplicate entries

--- lib/jekyll_geocode.rb
+++ lib/jekyll_geocode.rb
@@ -14,9 +14,12 @@ module Jekyll_Get
       #regEx for removing empty line
       regEx = /^[\s]*$\n/
 
+      #force locale
+      I18n.config.available_locales = :en
+
       #Get Geo service
       geo_service = 'https://nominatim.openstreetmap.org/?format=json&q='
-
+    
       #Get Config
       config = site.config['jekyll_geocode']
 
@@ -45,9 +48,9 @@ module Jekyll_Get
       #Path
       path_yaml = "#{data_source}/#{outputfile}"
 
-      if File.file?(path_yaml) && outputfile
-        File.open(path_yaml, 'w') {|file| file.truncate(0) }
-      end
+      # if File.file?(path_yaml) && outputfile
+      #   File.open(path_yaml, 'w') {|file| file.truncate(0) }
+      # end
 
       # Load YML file
       members = YAML.load_file("#{data_source}/#{filename}")
@@ -56,7 +59,7 @@ module Jekyll_Get
       members.each do |d|
         # Test if a JSON file exists for performance issues
         if !File.file?("#{data_source}/#{d[geo_name]}.json")
-          geo_name_field = I18n.transliterate(d[geo_name]).downcase.tr(" ", "-")
+          geo_name_field = d[geo_name].downcase.tr(" ", "-")
           if d[geo_postcode]
             geo_postcode_field = ",#{d[geo_postcode]}"
           end
@@ -75,8 +78,8 @@ module Jekyll_Get
           # Loop for an YML output
           if outputfile
             source.each do |coordinates|
-              data = [ "title" => "#{d[geo_name]}", "location" => { "latitude" => "#{coordinates["lat"]}","longitude" => "#{coordinates["lon"]}" } ]
-              data_yml = data.to_yaml.gsub("---", "").gsub(regEx, '')
+              data = [ "title" => "#{d[geo_name]}", "url" => "#places-01", "data_set" => "01", "location" => { "latitude" => "#{coordinates["lat"]}","longitude" => "#{coordinates["lon"]}" } ]
+              data_yml = data.to_yaml
               # Test if there is any yaml files and create file
               if !File.file?(path_yaml)
                 File.open(path_yaml, "w") {|f| f.write(data_yml) }
@@ -84,8 +87,13 @@ module Jekyll_Get
               # Test if there is yaml files and add data recursively
               if File.file?(path_yaml)
                 File.open(path_yaml, 'a') { |f|
-                  f.puts data_yml
+                  data_yml_simple = data_yml.gsub("---", "").gsub(regEx, '')
+                  f.puts data_yml_simple
                 }
+                # Load YML file and remove duplicate entries
+                file_yaml = YAML.load(File.open(path_yaml))
+                file_yaml_uniq = file_yaml.uniq { |s| s.first }
+                File.open(path_yaml, "w") {|f| f.write(file_yaml_uniq.to_yaml) }
               end
             end
           # Loop for an JSON output
